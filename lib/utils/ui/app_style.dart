d65b2d9aef9b572a6893ded7fa498081f06d6aef
saikou <azamovhud007@gmail.com>
2024-01-02 18:19:38 +0500

    added add screen

---
+++ lib/utils/ui/app_style.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppStyle {
+  AppStyle._();
+
+  //Orange
+  static TextStyle styleBlackPopinsW700 = GoogleFonts.poppins(
+    fontSize: 18,
+    fontWeight: FontWeight.w700,
+  );
+}
